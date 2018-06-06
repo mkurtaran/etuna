e6de15cc25b0cb68856d460bd73345fb0aef31df
Mertcan Kurtaran <kureseltikinma@gmail.com>
2018-06-06 14:50:12 +0300

    Auto-generated commit

--- src/app/services/message.model.ts
+++
@@ -1,7 +0,0 @@
-export class Message {
-    //key: string;
-    userUid: string;
-    message: string;
-    createDate: number;
-    updateDate: number;
-}
--- src/app/ui-compenents/datagrid/model/column.ts
+++
@@ -1,7 +0,0 @@
-export class Column {
-  public Name: string;
-  public Value: string;
-  public IsEditable?: boolean = false;
-  public IsDate?: boolean = false;
-  public Align?: string = "text-left";
-}
